1362a16cd94e74b7cf70925d0c2fec43142c06d8
Hua777 <liaoweihua.777@gmail.com>
2018-09-20 11:45:42 +0800

    課程爬蟲

---
+++ crawl.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Hua777"
+__copyright__ = "Copyright 2018, Hua777"
+__version__ = "2.0"
+__email__ = "liao.700529@gmail.com"
+
+from ClassesDB import ClassesDB as DB
+
+db = DB()
+
+db.Crawl()
